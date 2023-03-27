6739803f35ac19ed79af20a4a1f7e259adcf8706
Nouh <muhammadnour963963@gmail.com>
2023-03-27 18:22:17 +0200

    debug

--- pages/output.js
+++ pages/output.js
@@ -20,7 +20,7 @@ function output() {
             <h1 className='text-2xl font-bold mx-auto w-fit my-8 '>Title Strip Board</h1>
 
             <div className='mx-auto p-4 flex justify-center '>
-                <button onClick={handlePrint} class="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded ms-3    ">
+                <button onClick={handlePrint} className="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded ms-3    ">
                     save and download
                 </button>
             </div>
